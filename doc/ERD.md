68854e3a025a671eedd78948602fbdc54b91627b
mshige1979 <mshige1979@ryuouen.net>
2021-11-21 00:01:48 +0900

    モデル拡張

---
+++ doc/ERD.md
@@ -0,0 +1,12 @@
+```mermaid
+erDiagram
+  users {
+      Int id
+    String email
+    String name
+    Int age
+    DateTime created_at
+    DateTime updated_at
+    }
+  
+```
--- prisma/seed.ts
+++ prisma/seed.ts
@@ -6,19 +6,36 @@ const userData: Prisma.UserCreateInput[] = [
     {
         name: 'hoge1',
         email: 'hoge1@example.com',
+        age: 20,
     },
     {
         name: 'hoge2',
         email: 'hoge2@example.com',
+        age: 43,
     },
     {
         name: 'hoge3',
         email: 'hoge3@example.com',
+        age: 19,
     },
 ]
 
 const transfer = async () => {
     const users = [];
+
+    // 削除用データ取得
+    const userAll = await prisma.user.findMany()
+    userAll.map((user) => {
+        // 削除
+        const deleteUsser = prisma.user.deleteMany({
+            where: {
+                id: user.id
+            }
+        })
+        users.push(deleteUsser);
+    })
+
+    // 登録用データ設定
     for (const u of userData) {
         const user = prisma.user.create({
             data: u,
